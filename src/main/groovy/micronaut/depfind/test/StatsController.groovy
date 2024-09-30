4a3e8fdf3096b62d7ad5d809f6edeb1e61a2a9b9
Jean Tessier <jean@jeantessier.com>
2024-09-29 19:03:34 -0700

    Add endpoint to get metadata about the current graph

---
+++ src/main/groovy/micronaut/depfind/test/StatsController.groovy
@@ -0,0 +1,22 @@
+package micronaut.depfind.test
+
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import jakarta.inject.Inject
+
+@Controller("/stats")
+class StatsController {
+
+    final DependencyGraph graph
+
+    @Inject
+    StatsController(DependencyGraph graph) {
+        this.graph = graph
+    }
+
+    @Get
+    def index() {
+        graph.stats
+    }
+
+}
