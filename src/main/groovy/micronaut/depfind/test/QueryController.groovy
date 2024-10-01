e46060ba6e0f85569b46de42ab758b18fdfa2836
Jean Tessier <jean@jeantessier.com>
2024-09-30 22:02:17 -0700

    Streamline query controller
    For some reason, the `@Body` command parsing only works with `POST`
    requests.  I can't seem to get it to work with `GET` requests, which
    would make more sense since they don't modify the internal state of the
    server.

--- src/main/groovy/micronaut/depfind/test/QueryController.groovy
+++ src/main/groovy/micronaut/depfind/test/QueryController.groovy
@@ -2,10 +2,8 @@ package micronaut.depfind.test
 
 import com.jeantessier.dependency.JSONPrinter
 import io.micronaut.core.annotation.Introspected
-import io.micronaut.http.HttpParameters
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
 import io.micronaut.serde.annotation.Serdeable
 import jakarta.inject.Inject
@@ -20,46 +18,28 @@ class QueryController {
         this.graph = graph
     }
 
-    @Get
-    def get(HttpParameters parameters) {
-        def dependenciesQuery = graph.query(
-                parameters.get("packageScope"),
-                parameters.get("classScope"),
-                parameters.get("featureScope"),
-                parameters.getAll("scopeIncludes"),
-                parameters.getAll("scopeExcludes"),
-                parameters.get("packageFilter"),
-                parameters.get("classFilter"),
-                parameters.get("featureFilter"),
-                parameters.getAll("filterIncludes"),
-                parameters.getAll("filterExcludes"),
-        )
-
-        renderQuery(dependenciesQuery, parameters.get("showInbounds"), parameters.get("showOutbounds"), parameters.get("showEmptyNodes"))
-    }
-
     @Introspected
     @Serdeable.Deserializable
     static class QueryCommand {
         def packageScope = false
         def classScope = false
         def featureScope = false
-        def scopeIncludes = ["//"]
+        def scopeIncludes = []
         def scopeExcludes = []
 
         def packageFilter = false
         def classFilter = false
         def featureFilter = false
-        def filterIncludes = ["//"]
+        def filterIncludes = []
         def filterExcludes = []
 
-        def showInbounds = "on"
-        def showOutbounds = "on"
-        def showEmptyNodes = "on"
+        def showInbounds = false
+        def showOutbounds = false
+        def showEmptyNodes = false
     }
 
     @Post
-    def post(@Body QueryCommand command) {
+    def index(@Body QueryCommand command) {
         def dependenciesQuery = graph.query(
                 command.packageScope,
                 command.classScope,
@@ -73,18 +53,11 @@ class QueryController {
                 command.filterExcludes,
         )
 
-        renderQuery(dependenciesQuery, command.showInbounds, command.showOutbounds, command.showEmptyNodes)
-    }
-
-    private static renderQuery(dependenciesQuery, showInbounds, showOutbounds, showEmptyNodes) {
         def out = new StringWriter()
         def printer = new JSONPrinter(new PrintWriter(out))
-
-        if (showInbounds != null || showOutbounds != null || showEmptyNodes != null) {
-            printer.showInbounds = showInbounds == "on" || Boolean.valueOf(showInbounds)
-            printer.showOutbounds = showOutbounds == "on" || Boolean.valueOf(showOutbounds)
-            printer.showEmptyNodes = showEmptyNodes == "on" || Boolean.valueOf(showEmptyNodes)
-        }
+        printer.showInbounds = command.showInbounds
+        printer.showOutbounds = command.showOutbounds
+        printer.showEmptyNodes = command.showEmptyNodes
 
         printer.traverseNodes(dependenciesQuery.scopeFactory.packages.values())
 
